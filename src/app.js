721a34f2f8ec6a26f75dc6150bddd54acb9a3e7d
FrancoRegg <138858381+FrancoRegg@users.noreply.github.com>
2023-08-17 08:08:23 +0000

    Proyecto acabado sin CSS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["Mi gato", "Mi canario", "Mi abuelo"];
+let action = ["se llevó", "se comió", "meó", "empujó", "mordió"];
+let what = [
+  "el teléfono",
+  "la lampara",
+  "el coche",
+  "la puerta",
+  "mi camiseta"
+];
+let when = [
+  "mientras volvía a casa",
+  "cuando acabé de trabajar",
+  "mientras estaba durmiendo",
+  "mientras hacía la compra"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let randomExcuses = () => {
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML =
+    who[whoIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    what[whatIndx] +
+    " " +
+    when[whatIndx];
 };
+
+window.onload = randomExcuses();
